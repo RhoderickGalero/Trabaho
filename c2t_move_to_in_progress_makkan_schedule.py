67d5ecfe8c9cefeb0051f9da0104511a2ba9c5b2
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-09-18 13:48:14 +0200

    Update c2t_move_to_in_progress_makkan_schedule.py

--- c2t_move_to_in_progress_makkan_schedule.py
+++ c2t_move_to_in_progress_makkan_schedule.py
@@ -28,10 +28,12 @@ def main():
     project_items = json.loads(list_items())
     #print (project_items)
     for item in project_items["items"]:
-        if item['content']['type'] == "Issue" and item['status'] == "Todo issue" and (item['assignees'] is not None or item['assignees'] not in item):
-            #move_to_in_progress(item['id'])
-            print(f"Moved Issue {item['id']} to 'InProgress'")
-            #break
+        if item['assignees'] not in item):
+            print(" No issue in assigned")
+            else if item['content']['type'] == "Issue" and item['status'] == "Todo issue" and item['assignees'] is not None:
+                #move_to_in_progress(item['id'])
+                print(f"Moved Issue {item['id']} to 'InProgress'")
+                #break
 
 
 if __name__ == "__main__":
