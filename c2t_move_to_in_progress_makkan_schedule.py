faebc443fc32b76d7b7a24547096b150fe890ff1
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-09-18 12:34:08 +0200

    Update and rename c2t_move_to_in_progress_makkan.py to c2t_move_to_in_progress_makkan_schedule.py

--- c2t_move_to_in_progress_makkan_schedule.py
+++ c2t_move_to_in_progress_makkan_schedule.py
@@ -4,12 +4,12 @@ import os
 
 
 def list_items():
-    cmd = 'gh project item-list 8 --owner RhoderickGalero --format json'
+    cmd = 'gh project item-list 6 --owner RhoderickGalero --format json'
     return run_cmd(cmd)
 
 
 def move_to_in_progress(issue_id):
-    cmd = 'gh project item-edit --id {0} --field-id "PVTSSF_lAHOBbg8Ns4AT02azgMqiD8" --project-id  PVT_kwHOBbg8Ns4AVCc- --single-select-option-id "47fc9ee4"'.format(issue_id)
+    cmd = 'gh project item-edit --id {0} --field-id "PVTSSF_lAHOBbg8Ns4AT02azgMqiD8" --project-id PVT_kwHOBbg8Ns4AT02a --single-select-option-id "47fc9ee4"'.format(issue_id)
     return run_cmd(cmd)
     
 
@@ -24,13 +24,14 @@ def run_cmd(cmd):
 
 
 def main():
-    issue_nr = int(os.getenv("ISSUE_NR"))
+    #issue_nr = int(os.getenv("ISSUE_NR"))
     project_items = json.loads(list_items())
+    #print (project_items)
     for item in project_items["items"]:
-        if item['content']['type'] == "Issue" and item['content']['number'] == issue_nr:
-            move_to_in_progress(item['id'])
-            print(f"Moved Issue #{issue_nr} to 'InProgress'")
-            break
+        if item['content']['type'] == "Issue" and item['status'] == "Todo" and item['assignees'] is not None:
+            #move_to_in_progress(item['id'])
+            print(f"Moved Issue {item['id']} to 'InProgress'")
+            #break
 
 
 if __name__ == "__main__":
