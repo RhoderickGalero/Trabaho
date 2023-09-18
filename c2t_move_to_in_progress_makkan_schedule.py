df907a24cdd5652517b18d51e8882ddd3d4463ca
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-09-18 12:38:28 +0200

    Update c2t_move_to_in_progress_makkan_schedule.py

--- c2t_move_to_in_progress_makkan_schedule.py
+++ c2t_move_to_in_progress_makkan_schedule.py
@@ -4,7 +4,7 @@ import os
 
 
 def list_items():
-    cmd = 'gh project item-list 6 --owner RhoderickGalero --format json'
+    cmd = 'gh project item-list 8 --owner RhoderickGalero --format json'
     return run_cmd(cmd)
 
 
