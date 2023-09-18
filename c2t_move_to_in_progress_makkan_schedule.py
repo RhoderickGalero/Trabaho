69330f1235336c59e2acb707dbf88c1e10b60dc6
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-09-18 12:40:43 +0200

    Update c2t_move_to_in_progress_makkan_schedule.py

--- c2t_move_to_in_progress_makkan_schedule.py
+++ c2t_move_to_in_progress_makkan_schedule.py
@@ -9,7 +9,7 @@ def list_items():
 
 
 def move_to_in_progress(issue_id):
-    cmd = 'gh project item-edit --id {0} --field-id "PVTSSF_lAHOBbg8Ns4AT02azgMqiD8" --project-id PVT_kwHOBbg8Ns4AT02a --single-select-option-id "47fc9ee4"'.format(issue_id)
+    cmd = 'gh project item-edit --id {0} --field-id "PVTSSF_lAHOBbg8Ns4AT02azgMqiD8" --project-id PVT_kwHOBbg8Ns4AVCc- --single-select-option-id "47fc9ee4"'.format(issue_id)
     return run_cmd(cmd)
     
 
@@ -26,7 +26,7 @@ def run_cmd(cmd):
 def main():
     #issue_nr = int(os.getenv("ISSUE_NR"))
     project_items = json.loads(list_items())
-    #print (project_items)
+    print (project_items)
     for item in project_items["items"]:
         if item['content']['type'] == "Issue" and item['status'] == "Todo" and item['assignees'] is not None:
             #move_to_in_progress(item['id'])
