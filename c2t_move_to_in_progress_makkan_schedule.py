1a99358a82bd63fbb693cd40622d01ada38fe748
RhoderickGalero <95960118+RhoderickGalero@users.noreply.github.com>
2023-10-12 17:22:58 +0200

    Update c2t_move_to_in_progress_makkan_schedule.py

--- c2t_move_to_in_progress_makkan_schedule.py
+++ c2t_move_to_in_progress_makkan_schedule.py
@@ -12,6 +12,9 @@ def move_to_in_progress(issue_id):
     cmd = 'gh project item-edit --id {0} --field-id "PVTSSF_lAHOBbg8Ns4AVCc-zgNcWS4" --project-id PVT_kwHOBbg8Ns4AVCc- --single-select-option-id "47fc9ee4"'.format(issue_id)
     return run_cmd(cmd)
     
+def move_back_to_Todo(issue_id):
+    cmd = 'gh project item-edit --id {0} --field-id "PVTSSF_lAHOBbg8Ns4AVCc-zgNcWS4" --project-id PVT_kwHOBbg8Ns4AVCc- --single-select-option-id "f75ad846"'.format(issue_id)
+    return run_cmd(cmd)
 
 def run_cmd(cmd):
     print('Running cmd "{0}"'.format(cmd))
@@ -32,8 +35,11 @@ def main():
            if item['content']['type'] == "Issue" and item['status'] == "Todo issue" and item['assignees'] is not None:
             move_to_in_progress(item['id'])
             print(f"{item['id']} will be Move to 'InProgress issue'")
+         elif item['content']['type'] == 'Issue' and item['status'] == 'In Progress issue':
+            move_back_to_Todo(item['id'])
+            print(f"{item['title']} will be Move back to 'To do issue'") 
         else:
-             print(" Issue is not assigned")
+             print(f"{item['title']} Issue is not assigned")
             #break
 
 
